858aed0a5510a40b9605681ee6a53beff149a194
hyginn <boris.steipe@utoronto.ca>
2017-02-16 18:25:26 -0500

    better test

--- tests/testthat/testHello.R
+++ tests/testthat/testHello.R
@@ -4,7 +4,7 @@
 context("Demo test")
 
 test_that("hello.R returns a vector with the first seven Fibonacci numbers", {
-    expect_equal(sum(hello()), 33)
+    expect_identical(hello(), c(1, 1, 2, 3, 5, 8, 13))
 })
 
 # [END]
