1551adaa9a190f5cde95401ac055e38bddb90266
Myninn <myninn@Yi.local>
2017-02-16 23:40:13 +0100

    change print() to cat() to avoid "[1]"

--- R/hello.R
+++ R/hello.R
@@ -1,11 +1,11 @@
 # hello.R
-#
-# This is an example function which prints 'Hello, world!' and returns the first
-# seven Fibonacci numbers.
+
+# This is an example function which writes 'Hello, world!' to console, and
+# returns the first seven Fibonacci numbers.
 #
 
 hello <- function() {
-  print("Hello, world!")
+  cat("Hello, world!\n")
   return(c(1, 1, 2, 3, 5, 8, 13))
 }
 
