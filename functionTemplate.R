9f78da47ec264899b12247d370d120a1b77ce6f5
hyginn <boris.steipe@utoronto.ca>
2017-02-16 21:53:35 -0500

    Function template with Roxygen2 skeleton

---
+++ functionTemplate.R
@@ -0,0 +1,23 @@
+# functionTemplate.R
+
+#
+
+#' Title
+#'
+#' @param nDuck
+#' @param nCrow
+#' @param nGoose
+#'
+#' @return
+#' @export
+#'
+#' @examples
+myFunction <- function(nDuck, nCrow, nGoose) {
+    # ...
+
+    collective <- c(plump = nDuck, murder = nCrow, gaggle = nGoose)
+    return(collective)
+}
+
+
+# [END]
