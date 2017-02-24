b25b80fb5e7c6ffc5452aac0069a32fe420e11b5
hyginn <boris.steipe@utoronto.ca>
2017-02-24 18:04:40 -0500

    new file utils.R: placeholder function .appendToLog()

---
+++ R/utils.R
@@ -0,0 +1,25 @@
+# utils.R
+#
+# internal utility functions
+
+
+.appendToLog <- function(message) {
+    # Purpose:
+    #     Appends a message to the logfile
+    #
+    # Parameters:
+    #     message: string - the message
+    # Details:
+    #     TBD.
+    # Value:
+    #     NA: appends message to logfile as side-effect
+    # ToDo:
+    #     Everything.
+
+
+}
+
+
+
+
+# [END]
