466f1a2c277e87aae844fcaba573b8e5129a9033
Won June Tai <wonjune.tai@gmail.com>
2017-03-16 02:09:55 -0400

    Beginning annotate function

---
+++ R/annotate.R
@@ -0,0 +1,11 @@
+# Annotate Graphs (should this go into gGtools.R?)
+#
+#' Produce annotated gene graph where vertices have scores
+#' attributed to proper genes.
+#'
+#'
+#'
+#' @param gH List of genes and their associated score (mutational frequncy of
+#'           mutSigCV value)
+#' @param gG gene graph
+#' @return gG object with
---
+++ tests/testthat/testAnnotate.R
@@ -0,0 +1,8 @@
+# testAnnotate.R
+#
+#
+context("Gene graph annotation function")
+
+test_that("something", {
+
+})
