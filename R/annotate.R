498b5101ed6bffea0a1c222c59244d89af595002
Won June Tai <wonjune.tai@gmail.com>
2017-03-16 21:44:05 -0400

    Details for implementation
    - also adds tests and mostly working code
    - need to figure out logEvent
    - need to figure out one of the tests

--- R/annotate.R
+++ R/annotate.R
@@ -1,11 +1,99 @@
-# Annotate Graphs (should this go into gGtools.R?)
+# Annotate Graphs
 #
 #' Produce annotated gene graph where vertices have scores
-#' attributed to proper genes.
-#'
-#'
+#' attributed to genes.
 #'
 #' @param gH List of genes and their associated score (mutational frequncy of
 #'           mutSigCV value)
 #' @param gG gene graph
-#' @return gG object with
+#' @param silent Controls whether output to console should be suppressed. FALSE
+#'   by default.
+#' @param writeLog Controls whether writing the result to the global logfile is
+#'   enabled. TRUE by default.
+#' @return AGG object with annotated vertices
+#'
+#'   ## @examples ## \dontrun{
+#'     annotateGraph(gH, gG)
+#'   }
+#'
+#' @export
+annotateGraph <- function(gH, gG, silent = FALSE, writeLog = TRUE) {
+
+    gHName <- deparse(substitute(gH))
+    gGName <- deparse(substitute(gG))
+
+    # check for params - that they are not missing
+    if (is.null(gH)) {
+        stop(sprintf("Object \"%s\" is NULL.",
+                     gHName))
+    }
+
+    # TODO: Need to check for environment variable, not really objects... how?
+    # if (!exists(gH)) {
+    #     stop(sprintf("Environment variable \"%s\" not yet set."), gHName)
+    # }
+
+    if (is.null(gG)) {
+        stop(sprintf("Object \"%s\" is NULL.",
+                     gGName))
+    }
+
+    # == EXTRACT ALL VERTICES =================================================
+    allVertices <- igraph::vertex_attr(gG)$name
+
+    # == CREATED ORDERED VECTOR OF HEAT ASSIGNMENTS ===========================
+    numVerticesAnnotated <- 0
+    heatAssignments <- c()
+    for (i in 1:length(allVertices)) {
+        if (!is.null(gH[[allVertices[i]]])) {
+            heatAssignments[i] <- gH[[allVertices[i]]]
+        } else {
+            heatAssignments[i] <- 0
+        }
+
+        numVerticesAnnotated <- numVerticesAnnotated + 1
+
+    }
+    # == ATTACH CORRESPONDING VERTEX ANNOTATIONS TO AGG =========================
+    AGG <- igraph::set_vertex_attr(gG, name = "heat", value = heatAssignments)
+
+    # ==== SETUP METADATA ======================================================
+    meta <- list(type = "AGG",
+                 version = "1.0",
+                 UUID = uuid::UUIDgenerate())
+
+    if (!silent) {
+        # cat()
+    }
+
+    if (writeLog) {
+        logTitle <- "annotateGraph"
+
+        # Compile function call record
+        logCall <- character()
+        logCall[1] <- "annotateGraph("
+        logCall[2] <- sprintf("gH = \"%s\", ", as.character(gHName))
+        logCall[3] <- sprintf("gG = \"%s\", ", as.character(gGName))
+        logCall <- paste0(logCall, collapse = "")
+
+        # Record progress information
+        logNotes <- character()
+        logNotes <- c(logNotes, sprintf("Annotated %s vertices", numVerticesAnnotated))
+
+        # indicate output object name(s)
+        logOutput = c("AGG")
+
+        # send info to log file
+        # logEvent(eventTitle = logTitle,
+        #          eventCall = logCall,
+        #          notes = logNotes,
+        #          output = logOutput)
+    }
+
+    # ==== ATTACH METADATA =====================================================
+    for (name in names(meta)) {
+        attr(AGG, name) <- meta[[name]]
+    }
+
+    return(AGG)
+}
--- tests/testthat/testAnnotate.R
+++ tests/testthat/testAnnotate.R
@@ -3,6 +3,55 @@
 #
 context("Gene graph annotation function")
 
-test_that("something", {
+# Unknown: "ENSP00000005257", "ENSP00000003084"
+names = c("ENSP00000000233", "ENSP00000005340", "ENSP00000003100")
+heats = c(0.3, 0.5, 0.2)
+test_gH <- new.env(hash = TRUE)
+for (i in 1:length(names)) {
+    test_gH[[names[i]]] <- heats[i]
+}
+
+test_gG <- importNet.STRING(fName = "dataString.txt",
+                       net = "experimental",
+                       dropUnmapped = FALSE,
+                       silent = TRUE,
+                       writeLog = FALSE)
+
+test_that("annotateGraph rejects inappropriate objects", {
+    # test null gH object
+    null_gH <- NULL
+    expect_error(annotateGraph(gH = null_gH, gG = test_gG), "Object \"null_gH\" is NULL.")
+
+    # test null gG object
+    null_gG <- NULL
+    expect_error(annotateGraph(gH = test_gH, gG = null_gG), "Object \"null_gG\" is NULL.")
+
+    # test gH object is missing
+    expect_error(annotateGraph(gG = test_gG))
+
+    # test gG object is missing
+    expect_error(annotateGraph(gH = test_gH))
+})
+
+test_that("annotateGraph has added a new \"heat\" attribute", {
+    test_AGG <- annotateGraph(gH = test_gH, gG = test_gG)
+    # make sure it exists
+    expect_true(!is.null(test_AGG))
+    expect_true(!is.null(igraph::vertex_attr(test_AGG)$heat))
+})
+
+test_that("annotateGraph annotates graph vertices for which it has no score with 0", {
+    test_AGG <- annotateGraph(gH = test_gH, gG = test_gG)
+    expect_equal(igraph::vertex_attr(test_AGG)$heat[5], 0)
+})
+
+test_that("annotateGraph properly annotates the graph", {
+    # check each heat attribute in the graph to make sure it's truthy
+    test_AGG <- annotateGraph(gH = test_gH, gG = test_gG)
+    expect_equal(igraph::vertex_attr(test_AGG)$heat, c(0.3, 0.5, 0.2, 0.0, 0.0))
+})
+
+# think more about this one:
+test_that("annotateGraph does not produce a broken AGG if it receives a broken input", {
 
 })
