a8014fdcb509d1bc39fb9eeeff838e4080c25dd6
pruthv1 <desaipruthvi1@gmail.com>
2017-03-20 23:40:43 -0400

    fixed tests and implemented import module, complete!

--- R/importSNV.TCGA.R
+++ R/importSNV.TCGA.R
@@ -22,11 +22,14 @@
 #'
 #'   ## @examples ## \dontrun{ ## importSNV.TCGA(IN, OUT) ## }
 #' @export
+
 importSNV.TCGA <- function(  fMAF,
                              rSNV = NULL,
-                             na.rm = TRUE,
+                             na.rm = FALSE,
                              silent = FALSE,
                              writeLog = TRUE) {
+
+    myNotes <- character()
 
     # ==== PARAMETERS ==========================================================
 
@@ -39,7 +42,7 @@ importSNV.TCGA <- function(  fMAF,
     # General parameter checks
     cR <- character()
     cR <- c(cR, .checkArgs(fMAF,         like = rep("FILE_E", length(fMAF)),
-                                                            checkSize = TRUE))
+                                                             checkSize = TRUE))
     cR <- c(cR, .checkArgs(silent,       like = logical(1), checkSize = TRUE))
     cR <- c(cR, .checkArgs(writeLog,     like = logical(1), checkSize = TRUE))
     cR <- c(cR, .checkArgs(na.rm,        like = logical(1), checkSize = TRUE))
@@ -48,125 +51,160 @@ importSNV.TCGA <- function(  fMAF,
         stop(cR)
     }
 
-    #  Validate the vector of MAF file names parameter by checking
-    #  extension (.maf, .txt, .tsv. gz(?), .zip(?)) and header
+    meta <- list(type = "rSNV",
+                 version = "1.0")
 
-    #  Validate rSNV as an existing file
+    # ==== Validata fMAF: Extensions  =======================================
+    fileValidity <- function(file) {
+        if (strsplit(file, "\\.")[[1]][-1] %in% c("maf", "txt", "tsv")) {
+            return(TRUE)
+        } else {
+            return(FALSE)
+        }
+    }
+
+
+    # ==== Validata rSNV  =======================================
     if (missing(rSNV)) {
-        write("# metadata Start\n", "MAFtoSNV", append=FALSE)
-        # addMetadata <- function() {}
-        write("sym\tchr\tstart\tend\tstrand\tclass\ttype\taRef\ta1\ta2\tTCGA\tUUID", "MAFtoSNV", append=TRUE)
+        rSNV <- "MAFtoSNV"
+
+        # add metdata
+        for (name in names(meta)) {
+            attr(rSNV, name) <- meta[[name]]
+        }
+
+        write("sym\tchr\tstart\tend\tstrand\tclass\ttype\taRef\ta1\ta2\tTCGA\tUUID",
+              rSNV, append=TRUE)
     } else {
         rSNVf <- read.table(rSNV, header = TRUE)[,1]
         rowsBefore <- length(rSNVf)
+        myNotes <- c("Number of rows originally in rSNV - ", rowsBefore)
     }
-
-    myNotes <- c("Number of rows originally in rSNV - ", rowsBefore)
 
     # ==== READ DATA ===========================================================
-
+    filesFinished <- 0
     for (i in fMAF) {
+        if (fileValidity(i)) {
 
-        hugo_ <- read.table(i, header = TRUE, fill = NA, stringsAsFactors = FALSE)[,0]
-        chr_ <- read.table(i, header = TRUE, fill = NA, stringsAsFactors = FALSE)[,5]
-        start_ <- read.table(i, header = TRUE, fill = NA, stringsAsFactors = FALSE)[,6]
-        end_ <- read.table(i, header = TRUE, fill = NA, stringsAsFactors = FALSE)[,7]
-        strand_ <- read.table(i, header = TRUE, fill = NA, stringsAsFactors = FALSE)[,8]
-        var_class_ <- read.table(i, header = TRUE, fill = NA, stringsAsFactors = FALSE)[,9]
-        var_type_ <- read.table(i, header = TRUE, fill = NA, stringsAsFactors = FALSE)[,10]
-        ref_allele_ <- read.table(i, header = TRUE, fill = NA, stringsAsFactors = FALSE)[,11]
-        tum_all1_ <- read.table(i, header = TRUE, fill = NA, stringsAsFactors = FALSE)[,12]
-        tum_all2_ <- read.table(i, header = TRUE, fill = NA,  stringsAsFactors = FALSE)[,13]
-        tum_samp_barcode_ <- read.table(i, header = TRUE, fill = NA,  stringsAsFactors = FALSE)[,16]
-        tum_sam_uuid_ <- read.table(i, header = TRUE, fill = NA,  stringsAsFactors = FALSE)[,33]
+            hugo_ <- read.table(i, header = TRUE, fill = NA, stringsAsFactors = FALSE,
+                                na.strings = " ")[,1]
+            chr_ <- read.table(i, header = TRUE, fill = NA, stringsAsFactors = FALSE,
+                               na.strings = " ")[,5]
+            start_ <- read.table(i, header = TRUE, fill = NA, stringsAsFactors = FALSE,
+                                 na.strings = " ")[,6]
+            end_ <- read.table(i, header = TRUE, fill = NA, stringsAsFactors = FALSE,
+                               na.strings = " ")[,7]
+            strand_ <- read.table(i, header = TRUE, fill = NA, stringsAsFactors = FALSE,
+                                  na.strings = " ")[,8]
+            var_class_ <- read.table(i, header = TRUE, fill = NA, stringsAsFactors = FALSE,
+                                     na.strings = " ")[,9]
+            var_type_ <- read.table(i, header = TRUE, fill = NA, stringsAsFactors = FALSE,
+                                    na.strings = " ")[,10]
+            ref_allele_ <- read.table(i, header = TRUE, fill = NA, stringsAsFactors = FALSE,
+                                      na.strings = " ")[,11]
+            tum_all1_ <- read.table(i, header = TRUE, fill = NA, stringsAsFactors = FALSE,
+                                    na.strings = " ")[,12]
+            tum_all2_ <- read.table(i, header = TRUE, fill = NA,  stringsAsFactors = FALSE,
+                                    na.strings = " ")[,13]
+            tum_samp_barcode_ <- read.table(i, header = TRUE, fill = NA,  stringsAsFactors = FALSE,
+                                            na.strings = " ")[,16]
+            tum_sam_uuid_ <- read.table(i, header = TRUE, fill = NA,  stringsAsFactors = FALSE,
+                                        na.strings = " ")[,21]
 
-        # check if variant_class contains valid values from known list
+            # check if variant_class contains valid values from known list
+            classVknown <- c("Frame_Shift_Del", "Frame_Shift_Ins", "In_Frame_Del", "In_Frame_Ins", "Missense_Mutation",
+                        "Nonsense_Mutation", "Silent", "Splice_Site", "Translation_Start_Site", "Nonstop_Mutation",
+                        "3'UTR", "3'Flank", "5'UTR", "5'Flank", "IGR1" , "Intron", "RNA", "Targeted_Region")
+            myVclasses <- unique(var_class_)
+            if (! all(myVclasses %in% classVknown)) {
+                # drop file
+                classValid <- FALSE
+            } else {
+                classValid <- TRUE
+            }
 
-        classVknown <- c("Frame_Shift_Del", "Frame_Shift_Ins", "In_Frame_Del", "In_Frame_Ins", "Missense_Mutation",
-                    "Nonsense_Mutation", "Silent", "Splice_Site", "Translation_Start_Site", "Nonstop_Mutation",
-                    "3'UTR", "3'Flank", "5'UTR", "5'Flank", "IGR1" , "Intron", "RNA", "Targeted_Region")
-        myVclasses <- unique(var_class_)
-        if (! all(myVclasses %in% classVknown)) {
-            # drop file
-            classValid <- FALSE
-        } else {
-            classValid <- TRUE
+            # check if vairant_type contains valid values from known list
+            typeVknown <- c("SNP", "DNP", "TNP", "ONP", "INS", "DEL", "Consolidated2")
+            myVtypes <- unique(var_type_)
+            if (! all(myVtypes %in% typeVknown)) {
+                # drop file
+                typeValid <- FALSE
+            } else {
+                typeValid <- TRUE
+            }
+
+            # check chromosome value is valid
+            typeChr <- c("chr1","chr2","chr3","chr4","chr5","chr6","chr7","chr8","chr9","chr10","chr11","chr12","chr13",
+                         "chr14","chr15","chr16","chr17","chr18","chr19","chr20","chr21","chr22","chrX","chrY","chrM")
+            myVchr <- unique(chr_)
+            if (! all(myVchr %in% typeChr)) {
+                # drop file
+                chrValid <- FALSE
+            } else {
+                chrValid <- TRUE
+            }
+
+            numFilesInvalid <- 0
+
+            if (classValid & typeValid & chrValid == FALSE) {
+                # there was an invalid value
+                # report error in file and log
+                # drop file
+                # go to next
+                myNotes <- c(paste(i, " - File was dropped due to invalid attributes in column chr/class/type"))
+                numFilesInvalid <- numFilesInvalid + 1
+            } else {
+                # all tests passed
+                # add data to rSNV
+                fileasDT <- cbind(hugo_ , chr_ , start_, end_, strand_, var_class_,
+                                  var_type_, ref_allele_, tum_all1_, tum_all2_,
+                                  tum_samp_barcode_, tum_sam_uuid_)
+                if (na.rm) {
+                    na.omit(fileasDT)
+                }
+                write.table(fileasDT,  file=rSNV, sep="\t", append=TRUE, row.names =
+                                FALSE, col.names = FALSE, quote = FALSE)
+            }
+            filesFinished <- filesFinished + 1
         }
 
-        # check if vairant_type contains valid values from known list
-        typeVknown <- c("SNP", "DNP", "TNP", "ONP", "INS", "DEL", "Consolidated2")
-        myVtypes <- unique(var_type_)
-        if (! all(myVtypes %in% typeVknown)) {
-            # drop file
-            typeValid <- FALSE
-        } else {
-            typeValid <- TRUE
-        }
 
-        # check chromosome value
-        typeChr <- c("chr1","chr2","chr3","chr4","chr5","chr6","chr7","chr8","chr9","chr10","chr11","chr12","chr13",
-                     "chr14","chr15","chr16","chr17","chr18","chr19","chr20","chr21","chr22","X","Y","Z")
-        myVchr <- unique(chr_)
-        if (! all(myVchr %in% typeChr)) {
-            # drop file
-            chrValid <- FALSE
-        } else {
-            chrValid <- TRUE
-        }
+        # ==== WRITE LOG ===========================================================
 
-        # check start position and end are integers
-        if (classValid & typeValid & chrValid = FALSE) {
-            # there was an invalid value
-            # report error in file and log
-            # drop file
-            # go to next
-        } else {
-            # all tests passed
-            # add data to rSNV
-            fileasDT <- cbind(hugo_ , chr_ , start_, end_, strand_, var_class_,
-                              var_type_, ref_allele_, tum_all1_, tum_all2_,
-                              tum_samp_barcode_, tum_sam_uuid_)
-            write.table(fileasDT,  file=rSNV, sep="\t", append=TRUE, row.names =
-                            FALSE, col.names = FALSE, quote = FALSE)
+        if(writeLog) {
+
+            myTitle <- "importSNV.TCGA"
+
+            # Compile function call record
+            myCall <- character()
+            myCall[1] <- "importSNV.TCGA("
+            myCall[2] <- sprintf("fMAF = \"%s\", ", paste(fMAF, collapse=" "))
+            myCall[3] <- sprintf("rSNV = \"%s\", ", rSNV)
+            myCall[3] <- sprintf("silent = %s, ", as.character(silent))
+            myCall[4] <- sprintf("writeLog = %s)", as.character(writeLog))
+            myCall[5] <- sprintf("na.rm = %s)", as.character(na.rm))
+            myCall <- paste0(myCall, collapse = "")
+
+            # indicate input object name(s)
+            #   (NA for this importSNV.TCGA())
+
+            # Record progress information
+            myNotes <- c(myNotes, sprintf("Files processed - ", filesFinished))
+            myNotes <- c(myNotes, sprintf("Total number of files in input fMAF - ", length(fMAF)))
+            myNotes <- c(myNotes, sprintf("Files dropped due to incomptibility - "), numFilesInvalid)
+
+            # indicate output object name(s)
+            myOutput = c("rSNV")
+
+            # send info to log file
+            logEvent(eventTitle = myTitle,
+                      eventCall = myCall,
+                      input = character(fMAF, rSNV),
+                      notes = myNotes,
+                      output = myOutput)
         }
+        return(rSNV)
     }
-
-
-    # ==== WRITE LOG ===========================================================
-
-    if(writeLog) {
-
-        myTitle <- "importSNV.TCGA"
-
-        # Compile function call record
-        myCall <- character()
-        myCall[1] <- "importSNV.TCGA("
-        myCall[2] <- sprintf("fMAF = \"%s\", ", paste(fMAF, collapse=" "))
-        myCall[3] <- sprintf("rSNV = \"%s\", ", rSNV)
-        myCall[4] <- sprintf("silent = %s, ", as.character(silent))
-        myCall[5] <- sprintf("writeLog = %s)", as.character(writeLog))
-        myCall[6] <- sprintf("na.rm = %s)", as.character(na.rm))
-        myCall <- paste0(myCall, collapse = "")
-
-        # indicate input object name(s)
-        #   (NA for this importSNV.TCGA())
-
-        # Record progress information
-        myNotes <- character()
-        myNotes <- c(myNotes, sprintf("Files processed - ", nDone))
-
-        # indicate output object name(s)
-        myOutput = c("rSNV")
-
-        # send info to log file
-        logEvent(eventTitle = myTitle,
-                 eventCall = myCall,
-                 input = character(fMAF, rSNV),
-                 notes = myNotes,
-                 output = myOutput)
-    }
-
-    return(rSNV)
 }
 
 
--- tests/testthat/testImportSNV.TCGA.R
+++ tests/testthat/testImportSNV.TCGA.R
@@ -6,53 +6,54 @@ logFileName(fPath = tempdir(), setOption = TRUE)  # make tempdir() the log dir
 NL <- .PlatformLineBreak()
 # ==== END SETUP AND PREPARE ===================================================
 
-context("Tools for importing MAF files into a rSNV file")
+#context("Tools for importing MAF files into a rSNV file")
 options(stringsAsFactors=FALSE)
 
 test_that("parameter errors are correctly handled", {
     # fMAF is not provided
-    expect_error(importSNV())
+    expect_error(importSNV.TCGA())
     # fMAF is NULL/empty vector
-    expect_error(importSNV(fMAF = NULL))
+    expect_error(importSNV.TCGA(fMAF = NULL))
     # fMAF input vector contains non-string values
-    expect_error(importSNV(fMAF = 123))
+    expect_error(importSNV.TCGA(fMAF = 123))
     # file in fMAF could not be opened/does not exist
-    expect_error(importSNV(fMAF = c("nosuchfileMAF")))
+    expect_error(importSNV.TCGA(fMAF = c("nosuchfileMAF.maf")))
 
     # if rSNV parameter is provided, rSNV file does not exist
-    expect_error(importSNV(fMAF = testMAF, rSNV = "nosuchfileSNV"))
+    expect_error(importSNV.TCGA(fMAF = testMAF, rSNV = "nosuchfileSNV"))
     # if rSNV parameter is provided, but is NULL
-    expect_error(importSNV(fMAF = testMAF, rSNV = NULL))
+    expect_error(importSNV.TCGA(fMAF = testMAF, rSNV = NULL))
     # if rSNV parameter is provided, but is non-string value
-    expect_error(importSNV(fMAF = testMAF, rSNV = 123))
+    expect_error(importSNV.TCGA(fMAF = testMAF, rSNV = 123))
     # if rSNV is given vector input
-    expect_error(importSNV(fMAF = testMAF, rSNV = c("abc", "xyz")))
+    expect_error(importSNV.TCGA(fMAF = testMAF, rSNV = c("abc", "xyz")))
     # if rSNV is not provided, new file could not be created (file name conflict)
-    expect_error(importSNV(fMAF = testMAF))
+    expect_error(importSNV.TCGA(fMAF = testMAF))
 
     # if na.rm is not given logical input
-    expect_error(importSNV(na.rm = 1))
-    expert_error(importSNV(na.rm = NULL))
-    expert_error(importSNV(na.rm = logical()))
-    expert_error(importSNV(na.rm = c(TRUE, TRUE)))
+    expect_error(importSNV.TCGA(fMAF = testMAF,na.rm = 1))
+    expert_error(importSNV.TCGA(fMAF = testMAF,na.rm = NULL))
+    expert_error(importSNV.TCGA(fMAF = testMAF,na.rm = logical()))
+    expert_error(importSNV.TCGA(fMAF = testMAF,na.rm = c(TRUE, TRUE)))
 
     # if silent is not given logical input
-    expect_error(importSNV(silent = 1))
-    expert_error(importSNV(silent = NULL))
-    expert_error(importSNV(silent = logical()))
-    expert_error(importSNV(silent = c(TRUE, TRUE)))
+    expect_error(importSNV.TCGA(fMAF = testMAF,silent = 1))
+    expert_error(importSNV.TCGA(fMAF = testMAF,silent = NULL))
+    expert_error(importSNV.TCGA(fMAF = testMAF,silent = logical()))
+    expert_error(importSNV.TCGA(fMAF = testMAF,silent = c(TRUE, TRUE)))
 
     # write.log is not given logical input
-    expect_error(importSNV(writeLog = 1))
-    expert_error(importSNV(writeLog = NULL))
-    expert_error(importSNV(wrtieLog = logical()))
-    expert_error(importSNV(writeLog = c(TRUE, TRUE)))
+    expect_error(importSNV.TCGA(fMAF = testMAF,writeLog = 1))
+    expert_error(importSNV.TCGA(fMAF = testMAF,writeLog = NULL))
+    expert_error(importSNV.TCGA(fMAF = testMAF,wrtieLog = logical()))
+    expert_error(importSNV.TCGA(fMAF = testMAF,writeLog = c(TRUE, TRUE)))
 })
 
 test_that("a sane input gives an expected output with differnt input parameters", {
 
     # make small input (testIn)
-    testIn <- "~/Desktop/rete/testIn"
+    # have an input file, rows too big to import manually here (testIn)
+
     # make small output for that input manually (testOut)
     write("# metadata \n# 1 \n# 2  \n# 3 \n# 4 \n# 5 \n# metadata END\n", "testOut")
     header <- c("sym", "chr", "start", "end", "strand", "class", "type", "aRef", "a1", "a2", "TCGA", "UUID")
@@ -62,58 +63,53 @@ test_that("a sane input gives an expected output with differnt input parameters"
     sample3 <- c("VWA5B1", "chr1",	20319393,	20319393,	"+",	"Missense_Mutation",	"SNP", 	"C",	"C",	"T", "TCGA-QQ-A8VF-01A-11D-A37C-09", "2109690d-e8b1-4b5c-9a39-21af273324cf")
     sample4 <- c("TXLNA", "chr1",	32192719,	32192719,	"+",	"Silent",	"SNP",	"C",	"C",	"T", "TCGA-QQ-A8VF-01A-11D-A37C-09", "2109690d-e8b1-4b5c-9a39-21af273324cf")
     sample5 <- c("NFYC", "chr1",	40770537,	40770537,	"+",	"Silent",	"SNP",	"C",	"C",	"A", "TCGA-QQ-A8VF-01A-11D-A37C-09",	"2109690d-e8b1-4b5c-9a39-21af273324cf")
-    write(sample1, "testOut", ncolumns=12, sep="\t", append=TRUE)
-    write(sample2, "testOut", ncolumns=12, sep="\t", append=TRUE)
-    write(sample3, "testOut", ncolumns=12, sep="\t", append=TRUE)
-    write(sample4, "testOut", ncolumns=12, sep="\t", append=TRUE)
-    write(sample5, "testOut", ncolumns=12, sep="\t", append=TRUE)
-    trueOut <- read.csv("testOut", skip=7, sep = "\t")
+
+    trueOut <- rbind(sample1,sample2,sample3,sample4,sample5)
 
     # run the module on only fMAF as parameter (moduleOut)
-    moduleOut <- importSNV(c("testIn"))
+    moduleOut <- importSNV.TCGA(c("testIn"))
     # check if manual output = module output
     # skip 7 for metadata lines just for now
     module <- read.csv("moduleOut", skip=7, sep = "\t")
     all.equal(trueOut, module)
 
     # test input with rSNV as input
-    testInPre <- "~/Desktop/rete/testInPre"
+    #  have an input file, rows too big to import manually here (testInPre)
+
     # test output for rSNV as input
     sample1 <- c("VWA5B1", "chr1",	20319393,	20319393,	"+",	"Missense_Mutation",	"SNP", 	"C",	"C",	"T", "TCGA-QQ-A8VF-01A-11D-A37C-09", "2109690d-e8b1-4b5c-9a39-21af273324cf")
     sample2 <- c("TXLNA", "chr1",	32192719,	32192719,	"+",	"Silent",	"SNP",	"C",	"C",	"T", "TCGA-QQ-A8VF-01A-11D-A37C-09", "2109690d-e8b1-4b5c-9a39-21af273324cf")
     sample3 <- c("NFYC", "chr1",	40770537,	40770537,	"+",	"Silent",	"SNP",	"C",	"C",	"A", "TCGA-QQ-A8VF-01A-11D-A37C-09",	"2109690d-e8b1-4b5c-9a39-21af273324cf")
-    write(sample1, "testOutPre", ncolumns=12, sep="\t", append=TRUE)
-    write(sample2, "testOutPre", ncolumns=12, sep="\t", append=TRUE)
-    write(sample3, "testOutPre", ncolumns=12, sep="\t", append=TRUE)
-    trueOutPre <- read.csv("testOutPre", skip=7, sep = "\t")
+    trueOutPre <- rbind(sample1,sample2,sample3)
 
     # run the module on fMAF and rSNV as parameters
-    moduleOutPre <- importSNV(c("testInPre"), "module02")
+    moduleOutPre <- importSNV.TCGA(c("testInPre"), "module02")
     modulePre <- read.csv("moduleOutPre", skip=7, sep = "\t")
     # add to prexisting rSNV file
     all.equal(trueOutPre, modulePre)
 
     # run the module on fMAF and rSNV and silent as parameters
-    moduleOutPre <- importSNV(c("testInPre"), "module02", silent=TRUE)
+    moduleOutPre <- importSNV.TCGA(c("testInPre"), "module02", silent=TRUE)
     modulePre <- read.csv("moduleOutPre", skip=7, sep = "\t")
     # add to prexisting rSNV file
     all.equal(trueOutPre, modulePre)
 
     # run the module on fMAF, rSNV, silent and writeLog as parameters
-    moduleOutPre <- importSNV(c("testInPre"), "module02", silent=FALSE, writeLog=TRUE)
+    moduleOutPre <- importSNV.TCGA(c("testInPre"), "module02", silent=FALSE, writeLog=TRUE)
     modulePre <- read.csv("moduleOutPre", skip=7, sep = "\t")
     # add to prexisting rSNV file
     all.equal(trueOutPre, modulePre)
 
     # test input with rSNV as input and NA values
-    testInNA <- "~/Desktop/rete/testInNA"
+    #  have an input file, rows too big to import manually here (testInNA)
+
     # test output for rSNV as input and NA values
     sample1 <- c("VWA5B1", "chr1",	20319393,	20319393,	"+",	"Missense_Mutation",	"SNP", 	"C",	"C",	"T", "TCGA-QQ-A8VF-01A-11D-A37C-09", "2109690d-e8b1-4b5c-9a39-21af273324cf")
-    write(sample1, "testOutNA", ncolumns=12, sep="\t", append=TRUE)
-    trueOutNA <- read.csv("testOutNA", skip=7, sep = "\t")
+
+    trueOutNA <- rbind(sample1)
 
     # run the module on fMAF, rSNV, silent, writeLog and na.rm as parameters
-    moduleOutNA <- importSNV(c("testInNA"), "moduleNA", silent=FALSE, writeLog=FALSE, na.rm=TRUE)
+    moduleOutNA <- importSNV.TCGA(c("testInNA"), "moduleNA", silent=FALSE, writeLog=FALSE, na.rm=TRUE)
     moduleNA <- read.csv("moduleOutNA", skip=7, sep = "\t")
     # add to prexisting rSNV file
     all.equal(trueOutNA, moduleNA)
@@ -122,20 +118,20 @@ test_that("a sane input gives an expected output with differnt input parameters"
 
 test_that("a corrupt input does not lead to corrupted output", {
     # if fMAF file open does not follow the 2.4 or agreed upon version either STOP or DROP file
-    expect_error(importSNV(c("testIn2.4.1")))
+    expect_error(importSNV.TCGA(c("testIn2.4.1")))
     # if fMAF file open does not have the required columns at the
     #           right column number STOP or DROP file
-    expect_error(importSNV(c("testInWrongColumns")))
+    expect_error(importSNV.TCGA(c("testInWrongColumns")))
     # if rSNV is provided, check if number of columns in rSNV is worng
-    expect_error(importSNV(c("testIn")), "testOutINVALIDcolumns")
+    expect_error(importSNV.TCGA(c("testIn")), "testOutINVALIDcolumns")
     # if rSNV is provided, check when header labels are wrong
-    expect_error(importSNV(c("testIn")), "testOutINVALIDheaders")
+    expect_error(importSNV.TCGA(c("testIn")), "testOutINVALIDheaders")
 })
 
 test_that("silent and write works as intended", {
     # if silent=TRUE, check if output to console is supressed
     testF <- tempfile()
-    capture.output(importSNV(c("testIn"), silent = TRUE), file = testF)
+    capture.output(importSNV.TCGA(c("testIn"), silent = TRUE), file = testF)
     expect_equal(length(readLines(testF)), 0)
     expect_true(file.remove(testF))
 
