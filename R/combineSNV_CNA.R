a9e2c27a31e2c4b6f69bc8ff840451b838b3bb38
Charles Ding <charles.ding@mail.utoronto.ca>
2017-03-23 03:29:07 -0400

    fixed pseudo-code to use hash updated unit tests

--- R/combineSNV_CNA.R
+++ R/combineSNV_CNA.R
@@ -3,18 +3,18 @@
 #' Combine mutation data from CNA and SNV
 #'
 #' \code{combineSNV_CNA} loads the vector of preprocessed dataset from filtered CNA or SNV files,
-#'  creates hash keys corresponding to the input file type, combine and save the hashed
-#'  keys to a gX format file as output file.
+#'  creates hash keys corresponding to the input file type, combine the corresponding mutation count
+#'  to each gene and save to a gX format file as output file.
 #'
 #' Details.
 #' @section gX files:
-#'  gX data frame files contain data of SNV and CNA combined for each gene as one object, sorted per
+#'  gX rds files contain data of SNV and CNA combined for each gene as one object, sorted per
 #'  gene.
 #'
 #'
 #' @param fName A vector of path of fully qualified file names of SNV and/or CNA files.
 #' @param fgX The fully qualified filename/path of a gX output file, by default is gX.rds.
-#' @param silent Boolean option for writing combiling process information to console, FALSE by default.
+#' @param silent Boolean option for writing combining process information to console, FALSE by default.
 #' @param writeLog Boolean option for log results, TRUE by default.
 #'
 #'
@@ -23,29 +23,26 @@
 #'
 #' @export
 combineSNV_CNA <- function(fname, fgX = "gX.rds", silent=FALSE, writeLog=TRUE) {
+
     #' pseudo code
     #'
     #' using hash package, e.g extract keys
-    #' if(!require(hash)){
-    #'  install.packages("hash")
-    #'  library(hash)
-    #' }
-    #' hashTable <- new.env()
+    #' hashTable <- hash()
     #' read from input files and store hash
     #' for (i <- 1, i <= length(fname), i++) {
     #'  fcurrent <- fname[i]
     #'  if fcurrent is CNA {
-    #'      loadRDS(fcurrent)
-    #'      for each variation:
+    #'      CNAcurrent <- readRDS(fcurrent)
+    #'      for each variation in CNAcurrent:
     #'          key = hash(<gene symbol>:CNA:<as.character(round(<copy number>))>)
     #'          if key is in hashTable:
     #'              hashTable[[key]] <- hashTable[[key]] + 1
     #'          else:
     #'              hashTable[[key]] <- 1
     #'  }
-    #'  else if fcurrent is MUT {
-    #'      loadRDS(fcurrent)
-    #'      for each row:
+    #'  else if fcurrent is SNV {
+    #'      SNVcurrent <- readRDS(fcurrent)
+    #'      for each row in SNVcurrent:
     #'          key = hash(<gene symbol>:SNV:<position>:<variant class>)
     #'          if key is in hashTable:
     #'              hashTable[[key]] <- hashTable[[key]] + 1
@@ -65,7 +62,7 @@ combineSNV_CNA <- function(fname, fgX = "gX.rds", silent=FALSE, writeLog=TRUE) {
     #'  retrieve value from hashTable using keys in hashVector
     #'  insert metadata
     #'
-    #' save(dataframe, fgX)
+    #' saveRDS(dataframe, file=fgX)
     #'
     #' ToDo add silent and log to read from data files and writing to output files
 }
--- tests/testthat/testCombine.R
+++ tests/testthat/testCombine.R
@@ -6,20 +6,49 @@ context("test for Combine SNV and CNA files")
 # set up tempdir and tempfiles for output files and to be deleted after tests
 OLOG <- as.character(getOption("rete.logfile"))   # save original logfile name
 logFileName(fPath = tempdir(), setOption = TRUE)  # make tempdir() the log dir
+logName <- unlist(getOption("rete.logfile"))
+if (file.exists(logName)) { file.remove(logName)}
 # ==== END SETUP AND PREPARE ===================================================
 
 test_that("parameter errors are correctly handled", {
     # try no parameter input
-    expect_error(combineSNV_CNA(writeLog=FALSE))
+    expect_error(combineSNV_CNA(writeLog=FALSE),
+                 '.checkArgs> "fName" mode error')
+
     # try length-zero or NuLL input vector
     input_vector <- character()
-    expect_error(combineSNV_CNA(input_vector, writeLog=FALSE))
+    expect_error(combineSNV_CNA(input_vector, writeLog=FALSE),
+                 '.checkArgs> "fName" length error')
+
     # try wrong file format (e.g. text file instead of rds dataframe) in input vector
     input_vector[1] <- "CNA.txt"
-    expect_error(combineSNV_CNA(input_vector, writeLog=FALSE))
+    expect_error(combineSNV_CNA(input_vector, writeLog=FALSE),
+                 '.checkArgs> "fName" mode error')
+
+    # try non existing input file
+    input_vector[1] <- "nonExistent.rds"
+    expect_error(combineSNV_CNA(input_vector, writeLog=FALSE),
+                 '.checkArgs> "fName" error: file "NonExistent.rds" does not exist')
+
     # try invalid output fgX file name (e.g. wrong extension)
     input_vector[1] <- "CNA.rds"
-    expect_error(combineSNV_CNA(input_vector, "out.txt", writeLog=FALSE))
+    expect_error(combineSNV_CNA(input_vector, "out.txt", writeLog=FALSE),
+                 'invalid output file extension')
+
+    # try null output fgX file name
+    expect_error(combineSNV_CNA(input_vector, NULL, writeLog=FALSE),
+                 '.checkArgs> "fgx" mode error')
+
+    # try null silent option
+    expect_error(combineSNV_CNA(input_vector, silent=NULL, writeLog=FALSE),
+                 '.checkArgs> "silent" mode error')
+
+    # try null writeLog option
+    expect_error(combineSNV_CNA(input_vector, writeLog=NULL),
+                 '.checkArgs> "writeLog" mode error')
+
+    # check if log file has been written
+    expect_false(file.exists(logName))
 })
 
 
@@ -27,30 +56,38 @@ test_that("a sane input gives an expected output", {
     # provide one valid SNV and one valid CNA file with small number of genes as input
     # TODO where can I find the sample data for testing? inst/extdata folder has maf & txt formats
     input_vector = c("CNA.rds", "SNV.rds")
+    # testdir <- tempdir()
+    testF <- tempfile(fileext = ".rds")
+    # test_path = file.path(testdir, testF)
+    print(test_path)
 
     # test if a log file is saved if log is True
-    combineSNV_CNA(input_vector, writeLog=TRUE)
+    capture.output(combineSNV_CNA(input_vector, fgX=testF, silent=TRUE, writeLog=TRUE), file=testF)
     expect_true(files.exists(logName))
+
+    # correct log file
+    thisLog <- readLines(logName)
+    expect_true(grepl("event \\| title  \\| combineSNV_CNA",       thisLog[1]))
+    expect_true(grepl("event \\| time",                            thisLog[2]))
+
+    # call
+    expect_true(grepl("^event \\| call   \\| combineSNV_CNA\\(",   thisLog[3]))
+    expect_true(grepl(paste0("fname = \"", input_vector, "\""),    thisLog[3]))
+    expect_true(grepl("fgX = gX.rds",                              thisLog[3]))
+    expect_true(grepl("silent = TRUE",                             thisLog[3]))
+    expect_true(grepl("writeLog = TRUE",                           thisLog[3]))
+    expect_true(grepl(")$",                                        thisLog[3]))
+
+    expect_true(grepl("event | end",                               thisLog[4]))
+    expect_true(grepl("^$",                                        thisLog[5]))
+
     # test if output file fgX is saved under the path
-    combineSNV_CNA(input_vector, writeLog=FALSE)
-    expect_true(files.exists("gx.rds"))
-    # test if specified output filename is saved under the path
-    combineSNV_CNA(input_vector, fgx = "test.rds", writeLog=FALSE)
-    expect_true(files.exists("gx.rds"))
+    expect_true(files.exists(testF))
+    # remove tempfile
+    expect_true(file.remove(testF))
 
 })
 
-
-test_that("combine received corrupted input data file", {
-    # try SNV file with missing data (e.g. only gene symbol, no mutation class), expect error and stop in log and console
-    # TODO edit a CNA file to which some headers have empty/null data called corrupted_CNA.rds
-    input_vector = c("corrupted_CNA.rds", "SNV.rds")
-    expect_error(combineSNV_CNA(input_vector, writeLog=FALSE))
-    # try CNA file with missing copynumber, expect error and stop in log and console
-    # TODO edit a SNV file to which gene symbols correponds to null variant class & position called corrupted_SNV.rds
-    input_vector = c("CNA.rds", "corrupted_SNV.rds")
-    expect_error(combineSNV_CNA(input_vector, writeLog=FALSE))
-})
 
 # ==== BEGIN TEARDOWN AND RESTORE ==============================================
 logName <- unlist(getOption("rete.logfile"))
