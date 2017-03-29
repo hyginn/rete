200dba29428b215215d58a314b0be041ffceef13
hyginn <boris.steipe@utoronto.ca>
2017-03-29 17:11:07 -0400

    Added the closure isHGNCsymbol(), its factory function .fastCheckFactory() (in R/aaa.R), the script generateHGNCtable.R (in inst/scripts), the HGNC table it uses (in inst/extdata/HGNCsymbols.RDS) and its tests (in tests/testthat/testHGNCsymbol.R). The closure is created when R/isHGNCsymbol.R is being sourced.

---
+++ tests/testthat/testIsHGNCsymbol.R
@@ -0,0 +1,19 @@
+# testIsHGNCsymbol.R
+
+context("test the closure isHGNCsymbol()")
+
+test_that("expected input is correctly handled", {
+    expect_equal(isHGNCsymbol(), logical())
+    expect_equal(isHGNCsymbol(NULL), logical())
+    expect_false(isHGNCsymbol(0))
+    expect_true(isHGNCsymbol("A1BG"))                    # First in table
+    expect_true(isHGNCsymbol("a1bg"))                    # Case insensitive
+    expect_true(isHGNCsymbol("ZZZ3"))                    # Last in table
+    expect_equal(isHGNCsymbol(c(NA, "A1CF", NULL, "a2m")), c(FALSE, TRUE, TRUE))
+})
+
+test_that("unexpected input does not lead to output", {
+    expect_error(isHGNCsymbol(mean), "cannot coerce")
+})
+
+# [END]
