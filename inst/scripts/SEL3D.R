9d052e6761ccdc46ca5ce51cb651d8a3a261c7cf
Lindsay Liang <Lindsay@Lindsays-MacBook-Pro.local>
2017-04-01 17:23:11 -0400

    SEL3D stylistic changes and comments for clarity

--- inst/scripts/SEL3D.R
+++ inst/scripts/SEL3D.R
@@ -1,104 +1,134 @@
 #Pseudocode may look like actual code...please ignore...
-get_pdb_ids <- function(target_seq, start_pos = 1 , end_pos = nchar(target_seq), convert = TRUE, silent = TRUE){
+getPdbIds <- function(targetSeq, startPos = 1 , endPos = nchar(targetSeq), convert = TRUE, silent = TRUE){
   
   #get packages
-  # install.packages("bio3d")
-  # install.packages("RCurl")
-  # install.packages("XML")
-  # install.packages("seqinr")
-  # library(bio3d)
-  # library(RCurl)
-  # library(XML)
-  # library(seqinr)
-  
-  
+
+  library(bio3d)
+  library(RCurl)
+  library(XML)
+  library(seqinr)
   
   #convert sequence into AA 
   if(convert){
     #translate needs a vector of single chars, not a string
-    AA_seq <- paste(translate(s2c(target_seq)), collapse='')
-    end_pos <- nchar(AA_seq)
-  }else{ AA_seq <- target_seq }
+    AAseq <- paste(translate(s2c(targetSeq)), collapse='')
+    endPos <- nchar(AAseq)
+  }else{ AAseq <- targetSeq }
  
-  blast_output <- blast.pdb(AA_seq)
+  blastOutput <- blast.pdb(AAseq)
+
   
-  blast_output <- blast_output$hit.tbl
-  blast_output <- blast_output[blast_output$identity > 40, ]
+  #First we drop any match with ID < 40 to save runtime since we 
+  #don't want to include any of these results anyway (base case)
+  blastOutput <- blastOutput$hit.tbl
+  numEntriesBeforeIdentityDrop <- nrow(blastOutput)
+  blastOutput <- blastOutput[blastOutput$identity > 40, ]
+  numEntriesAfterIdentityDrop <- nrow(blastOutput)
   
-  if(nrow(blast_output) == 0){
+  message(sprintf("dropped %d entries because of identity <= 40", numEntriesBeforeIdentityDrop - numEntriesAfterIdentityDrop))
+  
+  if(nrow(blastOutput) == 0){
     return(NULL)
   }
   
-  blast_output$tax <- NA 
-  blast_output$info <- NA
+  blastOutput$tax <- NA 
+  blastOutput$info <- NA
   
-  final_output <- data.frame()
-  final_output_range <- c()
-  final_output_range_counter <- 1
+  finalOutput <- data.frame()
+  finalOutputRange <- c()
+  #Pointer to keep track of position to insert alignment ranges into final output
+  finalOutputRangePointer <- 1
   
-  for(entry in 1:nrow(blast_output)){
-    rcsb_id <- gsub("_", ".", blast_output$pdb.id[entry], ignore.case = TRUE)
-    lookup <- xmlParse(getURL(paste("http://www.rcsb.org/pdb/rest/describeMol?structureId=", rcsb_id, sep = "")))
+  for(entry in 1:nrow(blastOutput)){
+    #We're looking up taxonomy and the description of the pdbID
+    #from a different server which uses slightly different formatting
+    #Reformat PDB ID as output by BLAST to separate ID from chain with a "."
+    RCSBid <- gsub("_", ".", blastOutput$pdb.id[entry], ignore.case = TRUE)
+    entryXMLTable <- xmlParse(getURL(paste("http://www.rcsb.org/pdb/rest/describeMol?structureId=", RCSBid, sep = "")))
     
     #get taxonomy 
     
-    tax <- getNodeSet(lookup, '//Taxonomy/@name')[[1]][[1]]
-    info <- getNodeSet(lookup, '//polymerDescription/@description')[[1]][[1]]
+    #We don't want to drop all the non-homo sapien matches because of
+    #the edge case where a match could have 100% amino acid ID for a 
+    #non homo sapien which we want to include in the table.
+    #So we need to get all tax information as well as the protein
+    #information, without dropping anything solely based on tax
     
-    blast_output$tax[entry] <- ifelse(length(tax) > 0, tax, NA)
-    blast_output$info[entry] <- ifelse(length(info) > 0, yes = info, no = NA)
+    tax <- getNodeSet(entryXMLTable, '//Taxonomy/@name')[[1]][[1]]
+    info <- getNodeSet(entryXMLTable, '//polymerDescription/@description')[[1]][[1]]
     
-    if(!intersecting_positions(final_output_range, blast_output$q.start[entry], blast_output$q.end[entry])){
-      if((!is.na(blast_output$tax[entry]) && blast_output$tax[entry] == "Homo sapiens") || blast_output$identity[entry] == 100){
-        final_output <- rbind(final_output, blast_output[entry,])
-        final_output_range[[final_output_range_counter]] <- c(blast_output$q.start[entry]:blast_output$q.end[entry])
-        final_output_range_counter <- final_output_range_counter + 1
+    if (length(tax) > 0) {
+      blastOutput$tax[entry] <- tax
+    } else {
+      blastOutput$tax[entry] <- NA
+    }
+    
+    if(length(info) > 0) {
+      blastOutput$info[entry] <- info
+    } else {
+      blastOutput$info[entry] <- NA
+    }
+
+    
+    if(!intersectingPositions(finalOutputRange, blastOutput$q.start[entry], blastOutput$q.end[entry])){
+    # Finally, we then find the best match that is either Homo sapiens or 100% ID.
+      if((!is.na(blastOutput$tax[entry]) && blastOutput$tax[entry] == "Homo sapiens") || blastOutput$identity[entry] == 100){
+        finalOutput <- rbind(finalOutput, blastOutput[entry,])
+        finalOutputRange[[finalOutputRangePointer]] <- c(blastOutput$q.start[entry]:blastOutput$q.end[entry])
+        finalOutputRangePointer <- finalOutputRangePointer + 1
       }
     }
   }
   
-  if(nrow(final_output) == 0){ return(NULL) }
-  
-  final_output$q.start <- final_output$q.start + start_pos - 1
-  final_output$q.end <- final_output$q.end + start_pos - 1
-  
-  for(x in 1:length(final_output_range)){
-    final_output_range[[x]] <- final_output_range[[x]] + start_pos - 1
+  #no matches 
+  if(nrow(finalOutput) == 0){ 
+    message(sprintf("all %d matches were dropped, returning NULL", nrow(blastOutput)))  
+    return(NULL) 
   }
   
-  min_side <- min(final_output_range[[1]])
-  max_side <- max(final_output_range[[length(final_output_range)]])
+  finalOutput$q.start <- finalOutput$q.start + startPos - 1
+  finalOutput$q.end <- finalOutput$q.end + startPos - 1
   
-  if(end_pos - max_side > 50){
-    message(sprintf("running substr from pos %d to %d", max_side, end_pos))
-    max_frame <- get_pdb_ids(substr(AA_seq, max_side - start_pos, end_pos - start_pos), 
-                             start_pos = max_side, end_pos = end_pos, convert = FALSE, silent = silent)
-    final_output <- rbind(final_output, max_frame)
+  for(x in 1:length(finalOutputRange)){
+    finalOutputRange[[x]] <- finalOutputRange[[x]] + startPos - 1
   }
   
-  if(min_side > 50){
-    message(sprintf("running substr from pos %d to %d", start_pos, min_side))
-    min_frame <- get_pdb_ids(substr(AA_seq, 1, min_side - start_pos), 
-                             start_pos = start_pos, end_pos = min_side, convert = FALSE, silent = silent)
-    final_output <- rbind(final_output, min_frame)
+  minSide <- min(finalOutputRange[[1]])
+  maxSide <- max(finalOutputRange[[length(finalOutputRange)]])
+  
+  if(endPos - maxSide > 50){
+    message(sprintf("running substr from pos %d to %d", maxSide, endPos))
+    maxFrame <- getPdbIds(substr(AAseq, maxSide - startPos, endPos - startPos), 
+                             startPos = maxSide, endPos = endPos, convert = FALSE, silent = silent)
+    finalOutput <- rbind(finalOutput, maxFrame)
   }
   
-  for(y in 1:(length(final_output_range) - 1)){
-    left_side <- max(final_output_range[[y]])
-    right_side <- min(final_output_range[[y+1]])
+  if(minSide > 50){
+    message(sprintf("running substr from pos %d to %d", startPos, minSide))
+    minFrame <- getPdbIds(substr(AAseq, 1, minSide - startPos), 
+                             startPos = startPos, endPos = minSide, convert = FALSE, silent = silent)
+    finalOutput <- rbind(finalOutput, minFrame)
+  }
+  
+  for(y in 1:(length(finalOutputRange) - 1)){
+    leftSide <- max(finalOutputRange[[y]])
+    rightSide <- min(finalOutputRange[[y+1]])
     
-    if(right_side - left_side > 50){
-      message(sprintf("running substr from pos %d to %d", left_side, right_side))
-      inter_frame <- get_pdb_ids(substr(AA_seq, left_side - start_pos, right_side - start_pos), 
-                               start_pos = left_side, end_pos = right_side, convert = FALSE, silent = silent)
-      final_output <- rbind(final_output, inter_frame)
+    if(rightSide - leftSide > 50){
+      message(sprintf("running substr from pos %d to %d", leftSide, rightSide))
+      interFrame <- getPdbIds(substr(AAseq, leftSide - startPos, rightSide - startPos), 
+                               startPos = leftSide, endPos = rightSide, convert = FALSE, silent = silent)
+      finalOutput <- rbind(finalOutput, interFrame)
     }
   }
   
-  return(final_output)
+  return(finalOutput)
 }
-
-intersecting_positions <- function(r, st, ed){
+#We need to store ranges of the best alignments 
+#(as start (st) and end (ed) positions) so we can
+#add that match to the final output without adding
+#segments which overlap in the final format.
+intersectingPositions <- function(r, st, ed){
   for(x in 1:length(r)){
     if(length(intersect(r[[x]], c(st:ed))) > 0){
       return(TRUE)
@@ -106,3 +136,4 @@ intersecting_positions <- function(r, st, ed){
   }
   return(FALSE)
 }
+# [END]
