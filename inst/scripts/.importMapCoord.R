9bb19fb677188f69d2bb56298d6ec00d4fe76654
Sabrina Ge <sbrn.ge@gmail.com>
2017-04-01 22:13:51 -0400

    MakeMap now considers all condition except reverse genes; Brief validation

--- inst/scripts/.importMapCoord.R
+++ inst/scripts/.importMapCoord.R
@@ -1,38 +1,68 @@
 # <non-exported function header>
-# <check best practices>
 
+# mapCoord.R
+
+#' Title.
+#'
+#' \code{<function>} description.
+#'
+#' Details.
+#' @section <title>: Additional explanation.
+#'
+#' @param <p> <description>.
+#' @param <q> <description>.
+#' @return <description>.
+#'
+#' @family <optional description of family>
+#'
+#' @seealso \code{\link{<function>}} <describe related function>, ... .
+#'
+#' @examples
+#' multiply(1, 1)
+#' multiply(1i, 1i)
+#' multiply(13, 3)
 mapCoord <- function(geneData, dropExonBoundaries = TRUE, silent = FALSE, writeLog = TRUE){
-  library(parallel)
-  no_cores <- detectCores() - 1
-  cl <- makeCluster(no_cores, type = "PSOCK")
-    
-  
-  #might not work 
-  geneData$map <- parLapply(geneData, 1, function(x) makeMap(x))
-  
-  saveRDS(geneData, file = "example_data_with_functions.RDS")
-  
-  stopCluster(cl)
-}
+    library(parallel)
+    no_cores <- detectCores() - 1
+    cl <- makeCluster(no_cores, type = "PSOCK")
 
-makeMap <- function(gMod) {
-  # prepare the exon matrix
-  lG <- length(gMod)
-  exons <- matrix(numeric(lG - 2), ncol = 2)
-  exons[, 1] <- (gMod[2:(lG-1)])[c(TRUE, FALSE)]
-  exons[, 2] <- (gMod[3:(lG)])[c(TRUE, FALSE)]
+    makeMap <- function(ucscMod) {
 
-  # return a function that uses the exon matrix
-  return(
-    function(x) {
-      iEx <- which(x >= exons[ , 1] & x <= exons[ , 2])
-      if(length(iEx) == 0){ return(0) }
-      lPre <- sum((exons[ , 2] - exons [ , 1])[1:(iEx-1)])
-      lIn <- x - exons[iEx, 1] + 1
-      return(lPre + lIn)
+        # check for valid data
+        if(length(ucscMod$ExonStart) != length(ucscMod$ExonEnd)) { return(0) }
+
+        # prepare the exon matrix
+        exons <- cbind(ucscMod$ExonStart, ucscMod$ExonEnd)
+
+        # return a function that uses the exon matrix
+        return(
+            function(x) {
+                iEx <- which(x >= exons[ , 1] & x <= exons[ , 2])
+                if(length(iEx) != 1){ return(0) }
+                lPre <- sum((exons[ , 2] - exons [ , 1])[1:(iEx-1)])
+                lIn <- x - exons[iEx, 1] + 1
+                return(lPre + lIn)
+            }
+        )
     }
-  )
+
+    # "function factory" manufacturing step, parallelized
+    geneData$map <- parLapply(geneData, function(x) makeMap(x))
+
+    saveRDS(geneData, file = "genedata.rds")
+
+    stopCluster(cl)
 }
+
+# [END]
+
+
+# Validation of makeMap (function for one gene model )
+
+
+sample <- list(name="bob", start = 200, end = 300)
+sample$ExonStart <- c(201,220,240)
+sample$ExonEnd <- c(210,230,250)
 
 
 
